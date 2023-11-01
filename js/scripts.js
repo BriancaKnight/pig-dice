dd1c0ae35fe9af9181702c09b7a47e4d48de6faa
Brianca Knight <briancaknight@gmail.com>
2023-11-01 16:22:26 -0700

    Finish UI logic and HTML elements.
    Co-authored-by: Trent Dietzel <dietzeltrent@gmail.com> and Monica Barboza<MonBoza@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ function PigDice(player1, player2, rollScore) {
   this.currentPlayer = 1;
 }
 
-PigDice.prototype.rollDice = function () {
+PigDice.prototype.rollDice = function() {
     let currentRoll = Math.floor(Math.random() * 6) + 1;
     if (this.player1 + currentRoll + this.rollScore >= 100 ||  this.player2 + currentRoll + this.rollScore >= 100) {
         if (this.player1 + currentRoll + this.rollScore >= 100) {
@@ -21,24 +21,25 @@ PigDice.prototype.rollDice = function () {
         } else {
             this.rollScore = 0;
             this.switchPlayer();
+            this.currentScore();
         }
 
     return currentRoll;
 };
  
-PigDice.prototype.currentScore = function () {
-  return "Player 1 = " + this.player1 + " Player 2 = " + this.player2;
+PigDice.prototype.currentScore = function() {
+    playerSwitch();
 };
 
-PigDice.prototype.switchPlayer = function () {
+PigDice.prototype.switchPlayer = function() {
     if (this.currentPlayer === 1) {
-        return this.currentPlayer += 1;
+        this.currentPlayer += 1;
     } else {
-        return this.currentPlayer -= 1;
+        this.currentPlayer -= 1;
     }
 }
 
-PigDice.prototype.hold = function () {
+PigDice.prototype.hold = function() {
   if (this.currentPlayer === 1) {
     this.player1 += this.rollScore;
     this.rollScore = 0;
@@ -46,11 +47,29 @@ PigDice.prototype.hold = function () {
     this.player2 += this.rollScore;
     this.rollScore = 0;
    }
-    return this.switchPlayer();
+    this.switchPlayer();
 }
 
 // User Interface Logic
+let pigDice = new PigDice(0,0,0);
 
-let newGame = new PigDice(0, 0, 0);
-console.log("Current rollScore: " + newGame.rollDice());
-console.log(newGame)
+window.addEventListener("load", function(){
+    document.querySelector("button#roll").addEventListener("click", () => {
+        document.querySelector("p#player-switch").innerHTML = null;
+        const rollResult = pigDice.rollDice();
+        document.getElementById("new-roll").innerText = rollResult;
+        document.getElementById("current-player").innerText = pigDice.currentPlayer;
+        document.getElementById("counter").innerHTML = pigDice.rollScore;
+    });
+        document.querySelector("button#hold").addEventListener("click", () => {
+        pigDice.hold();
+        document.getElementById("player1").innerText = pigDice.player1;
+        document.getElementById("player2").innerText = pigDice.player2;
+        document.getElementById("current-player").innerText = pigDice.currentPlayer
+        document.getElementById("counter").innerHTML = pigDice.rollScore;
+    });
+})
+
+function playerSwitch() {
+    document.querySelector("p#player-switch").innerHTML = "Oops you rolled a 1... Next players turn.";
+}
