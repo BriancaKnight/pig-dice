e82d1a01c0c34691a2e9b497d9d5f7aa4af28d3c
Brianca Knight <briancaknight@gmail.com>
2023-11-01 10:15:31 -0700

    Pass test 3 creating dice roll.
    Co-authored-by: Trent Dietzel <dietzeltrent@gmail.com> and Monica Barboza<MonBoza@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,11 @@ function PigDice(score1, score2, rollScore) {
   this.rollScore = rollScore;
 }
 
+PigDice.prototype.rollDice = function () {
+  this.rollScore = Math.floor(Math.random() * 6) + 1;
+  return this.rollScore;
+}
+
 PigDice.prototype.currentScore = function () {
   return "Player 1 = " + this.score1 + " Player 2 = " + this.score2
 };
